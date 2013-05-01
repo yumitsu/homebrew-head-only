4dfda5ee82d9917c3700b4f53f7b59c0142e7be5
Michka Popoff <michkapopoff@gmail.com>
2013-05-01 19:26:54 +0200

    gccxml: new formula
    Closes #10.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

---
+++ gccxml.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Gccxml < Formula
+  homepage 'http://gccxml.github.io/'
+  head 'https://github.com/gccxml/gccxml/tarball/master'
+
+  depends_on 'cmake' => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make install"
+  end
+end
