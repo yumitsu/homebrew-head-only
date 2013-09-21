5352cb48995f8ebbcc5b33f67579f387b2ca6c09
Marcin Michalowski <h13ronim@gmail.com>
2013-09-21 13:04:29 +0200

    despotify: update with new upstream and missing dep
    Closes #18.
    
    Signed-off-by: Adam Vandenberg <flangy@gmail.com>

--- despotify.rb
+++ despotify.rb
@@ -1,13 +1,14 @@
 require 'formula'
 
 class Despotify < Formula
-  head 'https://despotify.svn.sourceforge.net/svnroot/despotify/src'
   homepage 'http://despotify.se/'
+  head 'https://svn.code.sf.net/p/despotify/code/src'
 
   depends_on 'pkg-config' => :build
   depends_on 'libao'
   depends_on 'libvorbis'
   depends_on 'mpg123'
+  depends_on 'libtool'
 
   def install
     system "make Makefile.local.mk"
