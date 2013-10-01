9dc13410ad73ad0253b83663a5e7871367dbeca5
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-10-01 14:16:52 -0700

    opentracker: move libowfat to separate formulae
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- opentracker.rb
+++ opentracker.rb
@@ -4,22 +4,9 @@ class Opentracker < Formula
   head 'cvs://:pserver:anoncvs:@cvs.erdgeist.org:/home/cvsroot:opentracker'
   homepage 'http://erdgeist.org/arts/software/opentracker/'
 
-  # http://www.fefe.de/libowfat/
-  resource 'libowfat' do
-    url 'http://dl.fefe.de/libowfat-0.28.tar.bz2'
-    sha1 '6bbe997ae1bbe94b784ab50a3d44b63a2e08d857'
-  end
+  depends_on 'libowfat' => :build
 
   def install
-    # First libowfat must be compiled and installed where opentracker is expecting it
-    libowfat_include = Pathname.new(pwd) + 'libowfat'
-    resource("libowfat").stage do
-      system "make", "install", "INCLUDEDIR=#{libowfat_include}", "LIBDIR=#{libowfat_include}", "MAN3DIR=."
-    end
-
-    # Tell opentracker that libowfat headers are located in the same directory as itself
-    ENV['PREFIX'] = '.'
-
     system "make", "opentracker"
     bin.install "opentracker"
   end
