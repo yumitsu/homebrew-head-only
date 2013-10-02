63fa96cd0bca06ff4daaba219e4ce690ae5eb46d
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-10-01 17:53:02 -0700

    opentracker: added several compile time tunables.
    Added options in same order as they defined in Makefile.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

--- opentracker.rb
+++ opentracker.rb
@@ -4,10 +4,28 @@ class Opentracker < Formula
   head 'cvs://:pserver:anoncvs:@cvs.erdgeist.org:/home/cvsroot:opentracker'
   homepage 'http://erdgeist.org/arts/software/opentracker/'
 
+  option "without-full-scrape", "Enable full scrape"
+  option "with-ipv6", "IPv6-only tracker"
+  option "with-sync-live", "Enable multicast sync"
+  option "with-ip-from-query", "Trust IP from query string"
+  option "with-gzip", "Use gzip if client requests it"
+  option "with-syslog", "Use syslog"
+  option "with-dev-random", "Use /dev/random"
+
   depends_on 'libowfat' => :build
 
   def install
-    system "make", "opentracker"
+    features = []
+    features << "-DWANT_FULLSCRAPE" if build.with? "full-scrape"
+    features << "-DWANT_V6" if build.with? "ipv6"
+    features << "-DWANT_SYNC_LIVE" if build.with? "sync-live"
+    features << "-DWANT_IP_FROM_QUERY_STRING" if build.with? "ip-from-query"
+    features << "-DWANT_COMPRESSION_GZIP" if build.with? "gzip"
+    features << "-DWANT_SYSLOGS" if build.with? "syslog"
+    features << "-DWANT_DEV_RANDOM" if build.with? "dev-random"
+    features = features.join(' ')
+
+    system "make", "opentracker", "FEATURES=#{features}"
     bin.install "opentracker"
   end
 end
