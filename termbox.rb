62259d065719ed8abfa56ede1ad4ca2081de51e0
thmzlt <thmzlt@users.noreply.github.com>
2014-06-24 14:25:58 -0300

    Add Termbox
    Closes #49.
    
    Signed-off-by: Jack Nagel <jacknagel@gmail.com>

---
+++ termbox.rb
@@ -0,0 +1,12 @@
+require "formula"
+
+class Termbox < Formula
+  homepage "https://code.google.com/p/termbox"
+  head "https://github.com/nsf/termbox.git"
+
+  def install
+    system "./waf", "configure", "--prefix=#{prefix}"
+    system "./waf"
+    system "./waf", "install"
+  end
+end
