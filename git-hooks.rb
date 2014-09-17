66935724a472f49d4f106a6bae85123695fe1a39
Jack Nagel <jacknagel@gmail.com>
2014-09-16 20:05:02 -0500

    git-hooks: drop caveats
    Use git hooks --help
    
    Closes #33

--- git-hooks.rb
+++ git-hooks.rb
@@ -1,25 +1,10 @@
-require 'formula'
+require "formula"
 
 class GitHooks < Formula
-
-  homepage 'https://github.com/icefox/git-hooks'
-  head 'https://github.com/icefox/git-hooks.git'
+  homepage "https://github.com/icefox/git-hooks"
+  head "https://github.com/icefox/git-hooks.git"
 
   def install
     bin.install "git-hooks"
   end
-
-  def caveats
-    <<-EOS.undent
-      Run 'git hooks --install' Inside a git project to change that projects
-      git hooks to use git-hooks hooks.  'git hooks --uninstall' can be run at
-      any time to go back to the old hooks that were installed before
-      (typically the default which does nothing).
-
-      Run 'git hooks --installglobal' to force any new git repository or any
-      git repository you clone to have a reminders to install git hooksi. (It
-      can't be on by default for security reasons of course)
-    EOS
-  end
-
 end
