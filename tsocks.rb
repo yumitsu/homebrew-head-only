b0800b8768bed4eb1cb765593af47e334209183d
Jack Nagel <jacknagel@gmail.com>
2014-09-16 20:50:39 -0500

    tsocks: modernize

--- tsocks.rb
+++ tsocks.rb
@@ -1,35 +1,24 @@
-require 'formula'
+require "formula"
 
 class Tsocks < Formula
   # The original is http://tsocks.sourceforge.net/
   # This GitHub repo is a maintained fork with OSX support
-  homepage 'https://github.com/pc/tsocks'
-  head 'https://github.com/pc/tsocks.git'
+  homepage "https://github.com/pc/tsocks"
+  head "https://github.com/pc/tsocks.git"
 
-  depends_on 'autoconf' => :build if MacOS.xcode_version.to_f >= 4.3
+  depends_on "autoconf" => :build
 
   def install
     system "autoconf", "-v"
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking", "--with-conf=#{config_file}"
+    system "./configure", "--disable-debug",
+                          "--prefix=#{prefix}",
+                          "--with-conf=#{etc}/tsocks.conf"
 
-    inreplace("tsocks") { |bin| bin.change_make_var! "LIBDIR", lib }
+    inreplace("tsocks") { |s| s.change_make_var! "LIBDIR", lib }
 
     system "make"
     system "make install"
-
-    etc.install "tsocks.conf.simple.example" => "tsocks.conf" unless config_file.exist?
-  end
-
-  def test
-    puts 'Your current public ip is:'
-    ohai `curl -sS ifconfig.me 2>&1`.chomp
-    puts "If your correctly configured #{config_file}, this should show the ip you have trough the proxy"
-    puts 'Your ip through the proxy is:'
-    ohai `tsocks curl -sS ifconfig.me 2>&1`.chomp
-  end
-
-  def config_file
-    etc / 'tsocks.conf'
+    etc.install "tsocks.conf.simple.example" => "tsocks.conf"
   end
 end
 
