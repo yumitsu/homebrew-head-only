960378bea39ec2b35c163aa905c0c1a4d4c7a129
Alex Zinchenko <yumitsu.kun@gmail.com>
2015-04-25 21:11:16 +0300

    Fix

--- dex.rb
+++ dex.rb
@@ -1,14 +1,16 @@
-require "formula"
-
 class Dex < Formula
   homepage "https://github.com/tihirvon/dex"
   head "https://github.com/tihirvon/dex.git"
 
   depends_on "homebrew/dupes/ncurses" => :optional
-  
+
   def install
     args = ["prefix=#{prefix}", "VERSION=head"]
     system "make", *args, "all"
     system "make", *args, "install"
+  end
+
+  test do
+    system bin/"dex", "-V"
   end
 end
